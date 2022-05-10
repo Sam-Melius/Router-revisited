0a80776ac1e453a4907c532ba913c3fcd63b97b0
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-10 14:49:12 -0700

    created list page

--- src/views/Characters/List.jsx
+++ src/views/Characters/List.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import { useEffect, useState} from 'react';
+import { useLocation, useHistory, Link } from 'react-router-dom';
 
 export default function List() {
+  const [characters, setCharacters] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const status = new URLSearchParams(location.search).get('status') ?? 'all';
+  const location = useLocation();
+  const history = useHistory();
+
+  const handleStatus = (event) => {
+    history.push(`/?status=${event.target.value}`);
+  };
+
+  useEffect(() => {
+    const getCharacters = async () => {
+        setLoading(true);
+  
+        const statusParams = new URLSearchParams(location.search).get('status');
+  
+        const url = statusParams === 'all' || !statusParams 
+        ? 'https://rickandmortyapi.com/api/character'
+        : `https://rickandmortyapi.com/api/character?status=${statusParams}`;
+        const res = await fetch(url);
+        const { results } = await res.json();
+        setCharacters(results);
+        setLoading(false);
+  
+      };
+      getCharacters();
+    }, [location.search]);
+
   return (
-    <div>List</div>
+    <>
+    <h1>List of Characters</h1>
+    {loading ? (
+      <p>Loading</p>
+    ) : (
+      <section>
+          <label htmlFor="status">Character status:</label>
+          <select id="status" value={status} onChange={handleStatus}>
+            <option value="all">All</option>
+            <option value="alive">Alive</option>
+            <option value="dead">Dead</option>
+            <option value="unknown">Unknown</option>
+          </select>
+          {characters.map((character) => (
+            <article key={character.id}>
+              <Link to={`/characters/${character.id}`}>
+                <h3>{character.name}</h3>
+                <p>Status: {character.status}</p>
+              </Link>
+            </article>
+          ))}
+        </section>
+    )}
+    </>
   )
 }
