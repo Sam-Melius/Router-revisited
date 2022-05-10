8db06fdfcf2d6510b122a22602f322be57afb59a
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-05-10 14:41:38 -0700

    setup routes for details and list

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { Route, Switch} from 'react-router-dom';
+import Detail from './views/Characters/Detail';
+import List from './views/Characters/List';
+
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Switch>
+      <Route path='/characters/:id'>
+        <Detail />
+      </Route>
+      <Route path='/'>
+        <List />
+      </Route>
+    </Switch>
+  )
 }
